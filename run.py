ce0d5b7b5538006b538e2dce6e6cb138c7775103
lisa-butler <l.butler1993@gmail.com>
2022-05-31 14:23:04 +0000

    'opening text for calculator added

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+print('Welcome to the Buehlmann Table for Air Diving Decompression')
+print('Use this table to calculate your Repetitive Dive time and Residual Nitrogen Times')
+
+calculating = True
+
+while calculating:
+    continue_calculaton = input('Would you like to start a calculation y/n')
+    if continue_calculation.lower() == 'y':
+        print('Input your depth and time below')
+    elif continue_calculation.lower() == 'n':
+        print('Exiting Calculator')
+        calculating = False
+    else:
+        print('That is not a valid input. Please try again')  
+
+print('Thanks for using the Dive Table Calculator')
