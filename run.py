4477026ef197a431bda5e737d1f332ba2fc3313f
lisa-butler <l.butler1993@gmail.com>
2022-06-01 09:36:53 +0000

    Added google docs to github

--- run.py
+++ run.py
@@ -1,16 +1,31 @@
 print('Welcome to the Buehlmann Table for Air Diving Decompression')
 print('Use this table to calculate your Repetitive Dive time and Residual Nitrogen Times')
 
-calculating = True
 
-while calculating:
-    continue_calculaton = input('Would you like to start a calculation y/n')
-    if continue_calculation.lower() == 'y':
-        print('Input your depth and time below')
-    elif continue_calculation.lower() == 'n':
-        print('Exiting Calculator')
-        calculating = False
-    else:
-        print('That is not a valid input. Please try again')  
 
-print('Thanks for using the Dive Table Calculator')
+
+continue_calculation = input('Would you like to start a calculation y/n')
+
+if continue_calculation.lower() == 'y':
+    depth = input('Depth:')
+    time = input('Time:')
+
+elif continue_calculation.lower() == 'n':
+    print('Exiting Calculator')
+   
+else:
+    print('That is not a valid input. Please try again')  
+
+
+
+
+"""
+Calculates if you need a decompression stop
+"""
+
+print('Your depth was {depth}')
+print('Your time was {time}')
+
+
+# if input('Depth:') = 15 and input('Time:') >= 90:
+#     print (deco)
